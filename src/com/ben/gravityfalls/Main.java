a0d6c9b361d196e92cb071c9757a5da08baef2e8
BuildTools <unconfigured@null.spigotmc.org>
2020-01-23 16:29:48 -0500

    Added config.yml and plugin.yml. Optimized cuboid selection algorithm.

--- src/com/ben/gravityfalls/Main.java
+++ src/com/ben/gravityfalls/Main.java
@@ -1,6 +1,7 @@
 package com.ben.gravityfalls;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -10,11 +11,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
 
 public class Main extends JavaPlugin implements Listener
 {
-	private ArrayList<Block> surroundingBlocks;
 	
 	@Override
 	public void onEnable()
@@ -30,6 +29,57 @@ public class Main extends JavaPlugin implements Listener
 		saveDefaultConfig();
 	}
 	
+	
+	private ArrayList<Block> buildXYStack(Location bottom)
+	{
+		ArrayList<Block> allXYs;
+		Collection<Block> allZs;
+		allXYs = new ArrayList<>();
+		allZs = new ArrayList<>();
+		
+		double radius = this.getConfig().getDouble("radius");
+		Location ptr = bottom;
+		
+		// Creates XY Square
+		for (double x = 0.0; x < radius * 2.0; x++)
+		{
+			for (double y = 0.0; y < radius * 2.0; y++)
+			{
+				ptr.add(x, y, 0.0);
+				allXYs.add(ptr.getBlock());
+			}
+		}
+		ptr = bottom;
+		
+		for (Block b : allXYs)
+		{
+			for (double z = 1.0; z < radius * 2.0; z++)
+			{
+				Location tempLocale = b.getLocation().add(0.0, 0.0, z);
+				allZs.add(tempLocale.getBlock());
+			}
+		}
+		
+		for (Block b : allZs)
+		{
+			allXYs.add(b);
+		}
+		
+		/* Creates Z-Deep Cube from the XY Squares
+		for (double x = 0.0; x < radius * 2; x++)
+		{
+			for (double z = 0.0; z < radius * 2; z++)
+			{
+				ptr.add(x, 0.0, z);
+				allXYs.add(ptr.getBlock());
+			}
+		}
+		*/
+		
+		return allXYs;
+	}
+	
+	
 	private void editSurroundingBlocks(Player player)
 	{
 		Location location = player.getLocation();
@@ -38,10 +88,16 @@ public class Main extends JavaPlugin implements Listener
 		double x1 = location.getX() - radius;
 		double y1 = location.getY() - radius;
 		double z1 = location.getZ() - radius;
-		double x2 = x1 + (radius * 2);
-		double y2 = y1 + (radius * 2);
-		double z2 = z1 + (radius * 2);
 		
+		Location bottom = new Location(player.getWorld(), x1, y1, z1);
+		ArrayList<Block> surroundingBlocks = buildXYStack(bottom);
+		
+		for (Block b : surroundingBlocks)
+		{
+			b.setType(Material.GLASS);
+		}
+		
+		/*
 		Location ptr;
 		for (double y = y1; y < y2; y++)
 		{
@@ -55,35 +111,6 @@ public class Main extends JavaPlugin implements Listener
 					ptrBlock.setType(Material.AIR);
 				}
 			}
-		}
-			
-		
-		/*
-		for (double i = x - 5; i < x + 5; x++)
-		{
-			for (double j = z - 5; j < z + 5; j++)
-			{
-				for (double k = y - 5; k < y + 5; y++)
-				{
-					Location loc = new Location(player.getWorld(), i, k, j);
-					Block ptrBlock = loc.getBlock();
-					
-					if (ptrBlock.getType() == Material.AIR ||
-						ptrBlock.getType() == Material.LAVA ||
-						ptrBlock.getType() == Material.WATER ||
-						ptrBlock.getType() == Material.OBSIDIAN ||
-						ptrBlock.getType() == Material.END_PORTAL ||
-						ptrBlock.getType() == Material.END_PORTAL_FRAME ||
-						ptrBlock.getType() == Material.END_STONE ||
-						ptrBlock.getType() == Material.BEDROCK)
-					{
-						return;
-					}
-					
-					;;
-				}
-			}
-			
 		}
 		*/
 	}
